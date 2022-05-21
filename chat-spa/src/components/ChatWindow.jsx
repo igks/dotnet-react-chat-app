aa1e38d3aec641855bf3754354c3b2a865c8929e
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-21 12:15:29 +0700

    create chat window

---
+++ chat-spa/src/components/ChatWindow.jsx
@@ -0,0 +1,41 @@
+import Message from "./Message";
+import ChatInput from "./ChatInput";
+
+const ChatWindow = ({ chats, currentUser }) => {
+  const chatsList = chats.map((chat) => (
+    <Message
+      key={Date.now() * Math.random()}
+      user={chat.user}
+      message={chat.message}
+      currentUser={currentUser}
+    />
+  ));
+
+  return (
+    <>
+      <div
+        style={{
+          height: "100vh",
+        }}
+      >
+        <div
+          style={{
+            height: "80vh",
+            maxHeight: "80vh",
+            overflow: "auto",
+            marginBottom: "20px",
+          }}
+        >
+          <h4>{currentUser}</h4>
+          <hr />
+          {chatsList}
+        </div>
+        <div style={{}}>
+          <ChatInput />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ChatWindow;
