4a7c916830d8297fcde526a75a91bdfa1e54f0c2
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2022-05-21 11:36:40 +0700

    add cors and map hub

--- chat.api/Program.cs
+++ chat.api/Program.cs
@@ -1,3 +1,5 @@
+using chat.api.SignalHub;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("ClientPermission", policy =>
+    {
+        policy.AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://localhost:3000")
+            .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -15,11 +29,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("ClientPermission");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHub<ChatHub>("/hub/chat");
 
 app.Run();
